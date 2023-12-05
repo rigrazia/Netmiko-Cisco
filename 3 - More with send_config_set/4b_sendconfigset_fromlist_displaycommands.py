b626c945b183b40bfb4fe0b29edecc7bf3faa016
Rick Graziani <graziani@cabrillo.edu>
2023-12-05 11:54:59 -0800

    Add files via upload

---
+++ 3 - More with send_config_set/4b_sendconfigset_fromlist_displaycommands.py
@@ -0,0 +1,67 @@
+'''
+Introduces how to make changes using the send_config_set() method and a list of configuration commands
+
+ConnetHandler() method: add secret password
+- enable() method: enter privileged mode
+- send_config_set() method: Configuration changes
+
+Uses network configured with 3-router scripts
+
+ChatGPT
+'''
+
+
+import netmiko
+
+device = '192.168.1.1'
+
+ipv6_interface_list = [
+    'ipv6 unicast-routing',
+    'interface g0/0',
+    'ipv6 address 2001:db8:cafe:1::1/64',
+    'ipv6 address fe80::1:1 link-local',
+    'exit',
+    
+    'interface g0/1',
+    'ipv6 address 2001:db8:cafe:2::1/64',
+    'ipv6 address fe80::1:2 link-local'
+]
+
+
+connection = netmiko.ConnectHandler(ip=device, 
+                                    device_type='cisco_ios', 
+                                    username='admin', 
+                                    password='cisco',
+                                    secret = 'class'
+                                    )
+
+
+
+# enable() method: Must use for privileged mode access unless the method does this for you.
+# The send_config_set() method that follows, requires the enable() method.
+# The send_config_set() method
+# In Netmiko, the enable() method is used to enter privileged exec mode (sometimes referred to as enable mode or enable password) on a network device. It is required when you need to execute commands that require elevated privileges, typically commands that modify the device's configuration.
+# This method checks whether you’re already in configuration mode. If you aren’t, it goes into config mode, executes the commands, and by default, exits configuration mode. 
+
+# Returns the output of the configuration commands, and you are storing that output in the variable output. Subsequently, you are printing the output to the console.
+# The output variable will contain the response from the network device for each command in the configuration set.
+connection.enable()        
+output = connection.send_config_set(ipv6_interface_list)
+print(output)
+        
+print(device)
+print('-'*11)
+print(connection.send_command('show ipv6 interface brief'))
+
+print('\n')
+
+connection.disconnect()
+    
+ 
+
+
+
+
+
+
+
